8079772570d3a2c0eb6e41ec43c84d15b8b987aa
Macbook 15 <edinorog1343@gmail.com>
2021-03-30 01:34:36 +0300

    add xcode project

---
+++ SssrSample_Metal/SssrSample_Metal/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  SssrSample_Metal
+//
+//  Created by Eugene on 30.03.2021.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
